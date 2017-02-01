e6b8dcc6365ac609e27ccdc6eb37b2f0031119df
devjem <android.devjem@gmail.com>
2017-02-01 01:43:53 -0600

    fixed blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ActiveRecord::Base
   has_and_belongs_to_many :comments
 
 
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}, uniqueness: true
+  validates :title, presence: true, length: {minimum: 3}, uniqueness: true
 	validates :user_id, presence: true
 	validates :content, presence: true, length: {minimum: 10}
 end
