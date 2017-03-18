6656e315cc2e03cb2a1370880bf36859aed00af9
devjem <android.devjem@gmail.com>
2017-03-18 05:00:08 -0500

    uploads to AWS and fixed player problems

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,6 @@ desc "Symlink shared config files"
 task :symlink_config_files do
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/secrets.yml #{ current_path }/config/secrets.yml"
-    run ". /etc/default/unicorn"
 end
 
 # after 'deploy:publishing', 'deploy:restart'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                      # required
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],  # required
+    aws_access_key_id:     "AKIAJEICQRLA266F7RGQ",                      # required
+    aws_secret_access_key: "TjHCnlmCFt3EcuS6CebO+9q7DFErtMemFTW8qyjW",  # required
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
